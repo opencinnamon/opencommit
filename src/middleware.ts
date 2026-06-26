59728c87d0023f731fe109e910d581ee557a7e79
kiirod <py@astrarune.com>
2026-06-26 21:27:41 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getIronSession } from "iron-session";
-import { AppSession } from "@/lib/session";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-
   if (req.nextUrl.pathname.startsWith("/start")) {
-    const session = await getIronSession<AppSession>(req.cookies, res.cookies, {
-      password: process.env.SESSION_SECRET as string,
-      cookieName: "opencommit_session",
-    });
-
-    if (!session.user) {
+    const cookie = req.cookies.get("opencommit_session");
+    if (!cookie?.value) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
 
-  return res;
+  return NextResponse.next();
 }
 
 export const config = {
