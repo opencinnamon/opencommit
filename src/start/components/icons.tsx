5f0cf289f729c06494bf545eedce92126287e6a4
kiirod <py@astrarune.com>
2026-06-26 21:13:40 +0100

    Create icons.tsx

---
+++ src/start/components/icons.tsx
@@ -0,0 +1,75 @@
+export function FolderIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="15" height="15" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M3 8.2C3 7.07989 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569 5.40973 4.09202 5.21799C4.51984 5 5.0799 5 6.2 5H9.67452C10.1637 5 10.4083 5 10.6385 5.05526C10.8425 5.10425 11.0376 5.18506 11.2166 5.29472C11.4184 5.4184 11.5914 5.59135 11.9373 5.93726L12.0627 6.06274C12.4086 6.40865 12.5816 6.5816 12.7834 6.70528C12.9624 6.81494 13.1575 6.89575 13.3615 6.94474C13.5917 7 13.8363 7 14.3255 7H17.8C18.9201 7 19.4802 7 19.908 7.21799C20.2843 7.40973 20.5903 7.71569 20.782 8.09202C21 8.51984 21 9.0799 21 10.2V15.8C21 16.9201 21 17.4802 20.782 17.908C20.5903 18.2843 20.2843 18.5903 19.908 18.782C19.4802 19 18.9201 19 17.8 19H6.2C5.07989 19 4.51984 19 4.09202 18.782C3.71569 18.5903 3.40973 18.2843 3.21799 17.908C3 17.4802 3 16.9201 3 15.8V8.2Z" stroke="#6b7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function CodeFileIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="15" height="15" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M7 8L3 11.6923L7 16M17 8L21 11.6923L17 16M14 4L10 20" stroke="#6b7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function DocFileIcon() {
+  return (
+    <svg viewBox="0 0 8.4666669 8.4666669" width="15" height="15" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <g transform="translate(0,-288.53332)">
+        <path d="M 5 1 A 1.0000999 1.0000999 0 0 0 4 2 L 4 30 A 1.0000999 1.0000999 0 0 0 5 31 L 27 31 A 1.0000999 1.0000999 0 0 0 28 30 L 28 9 A 1.0000999 1.0000999 0 0 0 27.707031 8.2929688 L 20.707031 1.2929688 A 1.0000999 1.0000999 0 0 0 20 1 L 5 1 z M 6 3 L 19.001953 3 L 19.001953 8.9980469 C 18.99982 9.5505004 19.445584 9.9999878 19.998047 10.001953 L 26 10.001953 L 26 29 L 6 29 L 6 3 z M 21.001953 4.4160156 L 24.587891 8.0019531 L 21.001953 8.0019531 L 21.001953 4.4160156 z" fill="#6b7280" transform="matrix(0.26458333,0,0,0.26458333,0,288.53332)"/>
+        <path d="m 2.8847656,291.70898 a 0.26495432,0.26495432 0 0 0 0.025391,0.5293 h 2.6464844 a 0.26465,0.26465 0 1 0 0,-0.5293 H 2.9101562 a 0.26460981,0.26460981 0 0 0 -0.025391,0 z" fill="#6b7280"/>
+        <path d="m 2.9101562,293.29492 a 0.26465,0.26465 0 1 0 0,0.5293 h 2.6464844 a 0.26465,0.26465 0 1 0 0,-0.5293 z" fill="#6b7280"/>
+        <path d="m 2.9101562,294.88281 a 0.26465,0.26465 0 1 0 0,0.5293 h 2.6464844 a 0.26465,0.26465 0 1 0 0,-0.5293 z" fill="#6b7280"/>
+      </g>
+    </svg>
+  );
+}
+
+export function NewFolderIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M9 13H15M12 10V16M12.0627 6.06274L11.9373 5.93726C11.5914 5.59135 11.4184 5.4184 11.2166 5.29472C11.0376 5.18506 10.8425 5.10425 10.6385 5.05526C10.4083 5 10.1637 5 9.67452 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V10.2C21 9.0799 21 8.51984 20.782 8.09202C20.5903 7.71569 20.2843 7.40973 19.908 7.21799C19.4802 7 18.9201 7 17.8 7H14.3255C13.8363 7 13.5917 7 13.3615 6.94474C13.1575 6.89575 12.9624 6.81494 12.7834 6.70528C12.5816 6.5816 12.4086 6.40865 12.0627 6.06274Z" stroke="#6b7280" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function NoMoreIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="14" height="14" xmlns="http://www.w3.org/2000/svg" fill="none" style={{ flexShrink: 0 }}>
+      <defs><style>{`.cls-1{fill:none;stroke:#6b7280;stroke-miterlimit:10;stroke-width:1.91px;}`}</style></defs>
+      <polygon className="cls-1" points="20.59 6.27 20.59 7.23 14.86 7.23 14.86 1.5 15.82 1.5 20.59 6.27"/>
+      <polygon className="cls-1" points="20.59 12 17.73 13.91 14.86 12 11.99 13.91 9.14 12 6.27 13.91 3.41 12 3.41 1.5 15.82 1.5 20.59 6.27 20.59 12"/>
+      <polygon className="cls-1" points="20.59 15.82 17.73 17.73 14.86 15.82 11.99 17.73 9.14 15.82 6.27 17.73 3.41 15.82 3.41 22.5 20.59 22.5 20.59 15.82"/>
+    </svg>
+  );
+}
+
+export function TrashIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M4 6H20L18.4199 20.2209C18.3074 21.2337 17.4512 22 16.4321 22H7.56786C6.54876 22 5.69264 21.2337 5.5801 20.2209L4 6Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M7.34491 3.14716C7.67506 2.44685 8.37973 2 9.15396 2H14.846C15.6203 2 16.3249 2.44685 16.6551 3.14716L18 6H6L7.34491 3.14716Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M2 6H22" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M10 11V16" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M14 11V16" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
+export function ErrorIcon() {
+  return (
+    <svg fill="#ffffff" viewBox="0 0 24 24" width="16" height="16" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M9,9a1,1,0,1,0,1,1A1,1,0,0,0,9,9Zm5.66,4.56-4.19,1.5A1,1,0,0,0,10.8,17a1,1,0,0,0,.34-.06l4.2-1.5a1,1,0,1,0-.68-1.88ZM15,9a1,1,0,1,0,1,1A1,1,0,0,0,15,9ZM12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"/>
+    </svg>
+  );
+}
+
+export function LoadMoreIcon() {
+  return (
+    <svg viewBox="0 0 24 24" width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
+      <path d="M12 4V20M4 12H20" stroke="#6b7280" strokeWidth="2" strokeLinecap="round"/>
+    </svg>
+  );
+}
