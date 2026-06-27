de2e27394c810383747b6a43caf1514f8815fb07
kiirod <py@astrarune.com>
2026-06-27 10:45:20 +0100

    Update page.tsx

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,19 +1,15 @@
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/session";
-import { getUserOrgs } from "@/lib/github";
 import StartClient from "./StartClient";
 
 export default async function StartPage() {
   const session = await getSession();
   if (!session.user) redirect("/");
 
-  const orgs = await getUserOrgs(session.user.accessToken).catch(() => []);
-
   return (
     <StartClient
       username={session.user.username}
       avatarUrl={session.user.avatarUrl}
-      orgs={orgs}
     />
   );
 }
